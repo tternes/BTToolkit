14a7fbe4ef378ebd0c2df311cb39b28df9f3f633
Thaddeus Ternes <thaddeus@bluetoo.co>
2014-02-01 19:05:43 -0600

    Initial Commit

---
+++ BTToolkit/BTToolkit.h
@@ -0,0 +1,13 @@
+//
+//  BTToolkit.h
+//  BTToolkit
+//
+//  Created by Thaddeus on 2/1/14.
+//  Copyright (c) 2014 Bluetoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BTToolkit : NSObject
+
+@end
---
+++ BTToolkitTests/BTToolkitTests.m
@@ -0,0 +1,34 @@
+//
+//  BTToolkitTests.m
+//  BTToolkitTests
+//
+//  Created by Thaddeus on 2/1/14.
+//  Copyright (c) 2014 Bluetoo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BTToolkitTests : XCTestCase
+
+@end
+
+@implementation BTToolkitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
